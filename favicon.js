998898116e73ee654ac2ff40bad8e22f24720971
vk448 <ak6339575@gmail.com>
2025-10-10 21:31:00 +0530

    favicon

---
+++ favicon.js
@@ -0,0 +1,6 @@
+// favicon.js
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = 'images/favicon.png'; // yahan apne icon ka naam likho
+document.head.appendChild(link);
